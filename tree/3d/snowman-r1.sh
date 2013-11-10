b42cbeb23298a5d8a4751f76087e020db80e44d0
Randy Glenn <randy@surrealitylabs.com>
2013-11-10 09:55:47 -0500

    Adding 3d renders from latest artwork, and removing some misplaced POVray files

--- tree/3d/snowman-r1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-"pvengine.exe" +W1600 +H1200 +A0.3 +FN +L"c:/windows/fonts" +L"/Users/picxpert/Projects/EagleStuff/ulp/eagle3d/ulp//../povray" +O"/Users/picxpert/Projects/xmas2013/tree/3d/snowman-r1.png" /Users/picxpert/Projects/xmas2013/tree/3d/snowman-r1.pov 
