586436d42dd92728d5ede761c6ff5f021daf88aa
Randy Glenn <randy@surrealitylabs.com>
2013-11-09 23:04:15 -0500

    Adding working snowman board and 3D renders of PCBs

---
+++ tree/3d/tree-r1.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"pvengine.exe" +W320 +H240 +FN +L"c:/windows/fonts" +L"/Users/picxpert/Projects/EagleStuff/ulp/eagle3d/ulp//../povray" +O"/Users/picxpert/Projects/xmas2013/tree/3d/tree-r1.png" /RENDER "/Users/picxpert/Projects/xmas2013/tree/3d/tree-r1.pov" 
