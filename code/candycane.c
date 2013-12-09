835f7ade9e4c861647870a1e46743793b3979c0c
Randy Glenn <randy@surrealitylabs.com>
2013-12-09 12:54:33 -0500

    Adding license for code (GPLv3) and ability for Makefile to operate in batch mode

--- code/candycane.c
+++ code/candycane.c
@@ -1,3 +1,19 @@
+/*
+This file is part of Xmas2013.
+
+Xmas2013 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Xmas2013 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Xmas2013.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "xmas_boardspecific.h"
 #include "xmas_common.h"
 #include "charlieplex.h"
